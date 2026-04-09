80e3c331c95903481f33a313c950f771de9d8902
rudra0700 <baruarudra7@gmail.com>
2026-04-09 08:05:00 +0600

    user routes updated

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -11,7 +11,7 @@ const router = express.Router();
 router.post("/register",validateRequest(createUserZodSchema), UserControllers.createUser);
 router.get("/all-user", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), UserControllers.getAllUsers);
 router.get("/me", checkAuth(...Object.values(Role)), UserControllers.getMe);
-router.patch("/:id", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), UserControllers.getSingleUser);
+router.get("/:id", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), UserControllers.getSingleUser);
 router.patch("/:id", validateRequest(updateUserZodSchema), checkAuth(...Object.values(Role)), UserControllers.updateUser);
 
 export const UserRoutes = router;
